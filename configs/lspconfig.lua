3d6082ab903221a42dfa0ac70fc6b225c33f54d8
farnsworth <farnsworth3010@proton.me>
2024-07-24 08:58:19 +0200

    angularlsp

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "stylelint_lsp" }
+local servers = { "html", "cssls", "angularls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -20,4 +20,13 @@ lspconfig.tsserver.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
+}
+
+lspconfig.stylelint_lsp.setup {
+  settings = {
+    filetypes = {
+      "css",
+      "scss",
+    },
+  },
 }
